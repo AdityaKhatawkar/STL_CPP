4ea52ef8951cd806f48f42e8a5f86387eb106ffb
AdityaKhatawkar <110993700+AdityaKhatawkar@users.noreply.github.com>
2024-04-18 00:06:32 +0530

    Update 09_Pair_Class.cpp

--- 09_Pair_Class.cpp
+++ 09_Pair_Class.cpp
@@ -1,4 +1,5 @@
 /*
+#include<utility> is the header file
 
 Pair is used to combine together two values that may be of different data types. 
 Pair provides a way to store two heterogeneous objects as a single unit. It is basically used if we want to store tuples.
